df72de8bb6eb0885d0a6cc5445ea8a8e5973d9b1
Andrea Canonica <andrea.canonica@ieffects.com>
2022-06-26 07:50:42 +0200

    Add LoginViewModel tests

--- app/src/test/java/com/example/composeloginzkb/LoginViewModelUnitTest.kt
+++ app/src/test/java/com/example/composeloginzkb/LoginViewModelUnitTest.kt
@@ -1,6 +1,9 @@
 package com.example.composeloginzkb
 
+import com.example.composeloginzkb.screen.login.LoginViewModel
+import org.junit.Assert.*
 import org.junit.Test
+import java.time.format.DateTimeFormatter
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,16 +13,55 @@ import org.junit.Test
 class LoginViewModelUnitTest {
     @Test
     fun viewModelValidatesNameCorrectly() {
-        //TODO
+        val viewModel = LoginViewModel()
+        viewModel.updateName("a")
+        viewModel.validate()
+
+        assertEquals("a", viewModel.name.value)
+        assertTrue(viewModel.isNameValid.value)
     }
 
     @Test
     fun viewModelValidatesEmailCorrectly() {
-        //TODO
+        val viewModel = LoginViewModel()
+        viewModel.updateEmail("a")
+        viewModel.validate()
+
+        assertEquals("a", viewModel.email.value)
+        assertFalse(viewModel.isEmailValid.value)
+
+        viewModel.updateEmail("a@a.c")
+        viewModel.validate()
+
+        assertFalse(viewModel.isEmailValid.value)
+
+        viewModel.updateEmail("a@a.ch")
+        viewModel.validate()
+
+        assertFalse(viewModel.isEmailValid.value)
     }
 
     @Test
     fun viewModelValidatesBirthdayCorrectly() {
-        //TODO
+        val viewModel = LoginViewModel()
+        val beforeDate = viewModel.beforeDate.plusYears(1)
+        val afterDate = viewModel.afterDate.minusYears(1)
+        val validDate = viewModel.beforeDate.minusYears(1)
+
+        viewModel.updateBirthday(beforeDate)
+        viewModel.validate()
+
+        assertEquals(beforeDate.format(DateTimeFormatter.ISO_LOCAL_DATE), viewModel.birthday.value)
+        assertFalse(viewModel.isBirthdayValid.value)
+
+        viewModel.updateBirthday(afterDate)
+        viewModel.validate()
+
+        assertFalse(viewModel.isBirthdayValid.value)
+
+        viewModel.updateBirthday(validDate)
+        viewModel.validate()
+
+        assertTrue(viewModel.isBirthdayValid.value)
     }
 }
