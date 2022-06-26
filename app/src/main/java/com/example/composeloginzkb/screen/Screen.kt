12794d6deb58f29b1c57551bd44dfce07184542d
Andrea Canonica <andrea.canonica@ieffects.com>
2022-06-26 07:49:59 +0200

    Remove unused parameter

--- app/src/main/java/com/example/composeloginzkb/screen/Screen.kt
+++ app/src/main/java/com/example/composeloginzkb/screen/Screen.kt
@@ -1,11 +1,8 @@
 package com.example.composeloginzkb.screen
 
-import androidx.annotation.StringRes
-import com.example.composeloginzkb.R
-
-sealed class Screen(val route: String, @StringRes val resourceId: Int) {
-    object LoginScreen : Screen("login", R.string.screen_login)
-    object HomeScreen : Screen("home", R.string.screen_home)
+sealed class Screen(val route: String) {
+    object LoginScreen : Screen("login")
+    object HomeScreen : Screen("home")
 
     fun withArgs(vararg args: String): String {
         return buildString {
